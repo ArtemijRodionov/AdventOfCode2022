d02913e5e7dbb6303983e295f75f770278402a08
Artemiy Rodionov <wertins@yandex-team.ru>
2022-12-02 00:31:26 +0300

    solve 1.2

--- 01.py
+++ 01.py
@@ -1,14 +1,18 @@
 import fileinput
+import heapq as hq
 
-max_value = 0
+max_heap = []
 value = 0
 for line in fileinput.input(encoding='utf-8'):
-    line = line.strip()
-    if not line:
-        max_value = max(max_value, value)
+    if not (line := line.strip()):
+        if len(max_heap) == 3:
+            hq.heappushpop(max_heap, value)
+        else:
+            hq.heappush(max_heap, value)
         value = 0
         continue 
     value += int(line)
  
-print(max_value)
+print(max(max_heap))
+print(sum(max_heap))
 
