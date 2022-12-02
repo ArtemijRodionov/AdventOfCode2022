7cee71f4a2ca9e49c51b872a096444ea9d7594f5
Artemiy Rodionov <wertins@yandex-team.ru>
2022-12-02 10:13:19 +0300

    solve 2.2

--- 02.py
+++ 02.py
@@ -1,22 +1,41 @@
 import fileinput
 
+# rock, paper, scissors
 move_scores = {'X': 1, 'Y': 2, 'Z': 3}
+lose = tuple(zip('ABC', 'ZXY'))
+draw = tuple(zip('ABC', 'XYZ'))
+win = tuple(zip('ABC', 'YZX'))
 incomes = {
-    # rock, paper, scissors
-    zip('ABC', 'ZXY'): 0,
-    zip('ABC', 'XYZ'): 3,
-    zip('ABC', 'YZX'): 6,
+    lose: 0,
+    draw: 3,
+    win: 6,
+}
+rules = {
+    # lose
+    'X': dict(lose),
+    # draw
+    'Y': dict(draw),
+    # win
+    'Z': dict(win),
 }
 income_scores = {}
 for options, score in incomes.items():
     for option in options:
         income_scores[option] = score
-total = 0
+
+total_first = 0
+total_second = 0
 for line in fileinput.input():
     abc, xyz = move = line.strip().split(' ', 1)
     move_score = move_scores[xyz]
     income_score = income_scores[tuple(move)]
-    total += move_score + income_score
+    total_first += move_score + income_score
 
-print(total)
+    ruled_xyz = rules[xyz][abc]
+    ruled_move_score = move_scores[ruled_xyz]
+    ruled_income_score = income_scores[(abc, ruled_xyz)]
+    total_second += ruled_move_score + ruled_income_score
+
+print(total_first)
+print(total_second)
 
