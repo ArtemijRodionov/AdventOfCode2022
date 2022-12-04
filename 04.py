b8564581e39015267577565b6cbade6b1b80a39a
Artemiy Rodionov <wertins@yandex-team.ru>
2022-12-04 15:26:14 +0300

    Solve 4.2

--- 04.py
+++ 04.py
@@ -1,11 +1,16 @@
 import fileinput
 
-count = 0
+full_overlap = 0
+overlap = 0
 for line in fileinput.input():
     l, r = line.split(',', 1)
     split_interval = lambda x: tuple(map(int, x.split('-', 1)))
     (ll, lr), (rl, rr) = split_interval(l), split_interval(r)
     if (ll <= rl and lr >= rr) or (ll >= rl and lr <= rr):
-        count += 1
+        full_overlap += 1
+        overlap += 1
+    elif max(ll, rl) <= min(lr, rr):
+        overlap += 1
 
-print(count)
+print(full_overlap)
+print(overlap)
