0bcdbec8e9ca985faf4604e4be571a614b7c8701
Artemiy Rodionov <wertins@yandex-team.ru>
2022-12-10 12:23:16 +0300

    solve 10.1

---
+++ 10.py
@@ -0,0 +1,66 @@
+import fileinput
+
+
+class CPU:
+    def __init__(self):
+        self.cycles = 0
+        self.x = 1
+
+    def __call__(self, op):
+        op(self)
+        self.cycles += op.cycles
+
+
+class Noop:
+    cycles = 1
+
+    def __call__(self, cpu):
+        pass
+
+
+class AddX:
+    cycles = 2
+
+    def __init__(self, n):
+        self.n = n
+
+    def __call__(self, cpu):
+        cpu.x += self.n
+
+
+class Marker:
+    def __init__(self, cpu):
+        self.cpu = cpu
+        self.signals = []
+
+        self._marks = [220, 180, 140, 100, 60, 20]
+        self._mark = self._marks.pop()
+
+    def __call__(self, op):
+        x = self.cpu.x
+        self.cpu(op)
+        if self._mark and self.cpu.cycles >= self._mark:
+            self.signals.append(x * self._mark)
+            self._mark = self._marks.pop() if self._marks else None
+
+
+def execute(ops):
+    cpu = Marker(CPU())
+    for op in ops:
+        cpu(op)
+    return cpu.signals
+
+ops = [] 
+for line in fileinput.input():
+    line = line.strip()
+    if line == 'noop':
+        ops.append(Noop())
+    elif line.startswith('addx'):
+        _, n = line.split(' ')
+        ops.append(AddX(int(n)))
+    else:
+        raise Exception()
+
+signals = execute(ops)
+print(signals)
+print(sum(signals))
