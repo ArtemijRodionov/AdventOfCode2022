27ceb170e8ba908fd2a7872aefd1d2401a9ef8c9
Artemiy Rodionov <wertins@yandex-team.ru>
2022-12-24 15:21:07 +0300

    solve 13.1

--- rust/day13/src/main.rs
+++ rust/day13/src/main.rs
@@ -1,9 +1,18 @@
-use std::{iter::Peekable, str::Chars};
+use std::{iter::Peekable, str::Chars, cmp::Ordering};
 
 #[derive(Debug)]
 enum Value {
     Interger(i64),
     List(Vec<Value>),
+}
+
+impl Value {
+    fn wrap(&self) -> Self {
+        match self {
+            Value::Interger(i) => Value::List(vec![Value::Interger(*i)]),
+            Value::List(of) => panic!("can't wrap list: {:?}", of),
+        }
+    }
 }
 
 type ParseIter<'a> = Peekable<Chars<'a>>;
@@ -13,8 +22,8 @@ fn parse_integer(buf: &mut ParseIter) -> Option<Value> {
 
     while let Some(chr) = buf.peek() {
         match chr {
-            '0'..='9'         => xs.push(buf.next().unwrap()),
-            _                 => break,
+            '0'..='9' => xs.push(buf.next().unwrap()),
+            _         => break,
         };
     }
 
@@ -41,11 +50,7 @@ fn parse_list(buf: &mut ParseIter) -> Option<Value> {
         };
     }
 
-    if list.is_empty() {
-        None
-    } else {
-        Some(Value::List(list))
-    }
+    Some(Value::List(list))
 }
 
 fn parse(buf: &mut ParseIter) -> Option<Value> {
@@ -66,16 +71,38 @@ fn parse(buf: &mut ParseIter) -> Option<Value> {
     }
 }
 
+fn cmp_value(lhs: &Value, rhs: &Value) -> Ordering {
+    match (lhs, rhs) {
+        (Value::Interger(l), Value::Interger(r))       => l.cmp(r),
+        (Value::List(l), Value::List(r)) => {
+            for i in 0..std::cmp::min(r.len(), l.len()) {
+                match cmp_value(&l[i], &r[i]) {
+                    Ordering::Equal => continue,
+                    pred @ Ordering::Less | pred @ Ordering::Greater => return pred,
+                }
+            }
+
+            return l.len().cmp(&r.len());
+        },
+        (i @ Value::Interger(_), xs) => cmp_value(&i.wrap(), xs),
+        (xs, i @ Value::Interger(_)) => cmp_value(&xs, &i.wrap()),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
-    let mut xs = "123,321,111".chars().peekable();
-    println!("{:?}", parse(&mut xs));
-    println!("{:?}", parse(&mut xs));
-    println!("{:?}", parse(&mut xs));
+    let content = std::fs::read_to_string(std::env::args().nth(1).expect("pass path to input")).expect("can't read input");
 
-    let mut xs = "[1,2,[2,[3,[4,[5,6,7]]]],8,9]".chars().peekable();
-    println!("{:?}", parse(&mut xs));
-
-    let mut xs = "[[4,4],4,4,4]".chars().peekable();
-    println!("{:?}", parse(&mut xs));
+    let mut result = 0;
+    for (i, xs) in content.split("\n\n").enumerate() {
+        let splitted: Vec<&str> = xs.split('\n').collect();
+        let mut lhs = splitted[0].chars().peekable();
+        let mut rhs = splitted[1].chars().peekable();
+        if let Ordering::Less = cmp_value(
+            &parse(&mut lhs).unwrap(),
+            &parse(&mut rhs).unwrap(),
+        ) {
+            result += i + 1;
+        }
+    }
+    println!("{}", result);
 }
