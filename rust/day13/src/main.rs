e03d1004580a3be59c452af151b3491e3f5e2ac3
Artemiy Rodionov <wertins@yandex-team.ru>
2022-12-24 16:21:43 +0300

    fmt

--- rust/day13/src/main.rs
+++ rust/day13/src/main.rs
@@ -1,7 +1,6 @@
-use std::{iter::Peekable, str::Chars, cmp::Ordering};
+use std::{cmp::Ordering, iter::Peekable, str::Chars};
 
-#[derive(Debug)]
-#[derive(Eq, Clone)]
+#[derive(Debug, Eq, Clone)]
 enum Value {
     Interger(i64),
     List(Vec<Value>),
@@ -24,14 +23,14 @@ fn parse_integer(buf: &mut ParseIter) -> Option<Value> {
     while let Some(chr) = buf.peek() {
         match chr {
             '0'..='9' => xs.push(buf.next().unwrap()),
-            _         => break,
+            _ => break,
         };
     }
 
     let val = xs.iter().collect::<String>();
     match val.parse::<i64>() {
         Ok(x) => Some(Value::Interger(x)),
-        _          => panic!("can't parse ingeger: {}", val),
+        _ => panic!("can't parse ingeger: {}", val),
     }
 }
 
@@ -46,7 +45,10 @@ fn parse_list(buf: &mut ParseIter) -> Option<Value> {
 
     while let Some(chr) = buf.peek() {
         match chr {
-            ']'     => { buf.next(); break; },
+            ']' => {
+                buf.next();
+                break;
+            }
             '[' | _ => parse(buf).map(|val| list.push(val)),
         };
     }
@@ -58,14 +60,14 @@ fn parse(buf: &mut ParseIter) -> Option<Value> {
     while let Some(chr) = buf.peek() {
         match chr {
             ' ' | ',' => buf.next(),
-            _         => break,
+            _ => break,
         };
     }
 
     if let Some(chr) = buf.peek() {
         match chr {
             '[' => parse_list(buf),
-            _   => parse_integer(buf),
+            _ => parse_integer(buf),
         }
     } else {
         None
@@ -75,7 +77,7 @@ fn parse(buf: &mut ParseIter) -> Option<Value> {
 impl Ord for Value {
     fn cmp(&self, other: &Self) -> Ordering {
         match (self, other) {
-            (Value::Interger(l), Value::Interger(r))       => l.cmp(r),
+            (Value::Interger(l), Value::Interger(r)) => l.cmp(r),
             (i @ Value::Interger(_), xs) => i.wrap().cmp(xs),
             (xs, i @ Value::Interger(_)) => xs.cmp(&i.wrap()),
             (Value::List(l), Value::List(r)) => {
@@ -86,8 +88,8 @@ impl Ord for Value {
                     }
                 }
 
-                return l.len().cmp(&r.len());
-            },
+                l.len().cmp(&r.len())
+            }
         }
     }
 }
@@ -105,27 +107,26 @@ impl PartialEq for Value {
 }
 
 fn main() {
-    let content = std::fs::read_to_string(std::env::args().nth(1).expect("pass path to input")).expect("can't read input");
+    let content = std::fs::read_to_string(std::env::args().nth(1).expect("pass path to input"))
+        .expect("can't read input");
 
-    let mut result = 0;
+    let mut right_order_idx_sum = 0;
     for (i, xs) in content.split("\n\n").enumerate() {
         let splitted: Vec<&str> = xs.split('\n').collect();
         let mut lhs = splitted[0].chars().peekable();
         let mut rhs = splitted[1].chars().peekable();
         if parse(&mut lhs).unwrap() < parse(&mut rhs).unwrap() {
-            result += i + 1;
+            right_order_idx_sum += i + 1;
         }
     }
-    dbg!(result);
+    dbg!(right_order_idx_sum);
 
     let parsed = content
         .split('\n')
         .filter(|x| !x.is_empty())
         .map(|x| parse(&mut x.chars().peekable()).unwrap());
 
-    let make_divider = |x| {
-        Value::List(vec![Value::List(vec![Value::Interger(x)])])
-    };
+    let make_divider = |x| Value::List(vec![Value::List(vec![Value::Interger(x)])]);
 
     let dividers = vec![make_divider(2), make_divider(6)];
     let mut packets: Vec<Value> = parsed.chain(dividers.iter().cloned()).collect();
